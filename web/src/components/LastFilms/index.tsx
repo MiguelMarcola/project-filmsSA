140db3d13a1e440d2465d7c8d83654a540bc32d7
MiguelMarcola <miguelmarcola@gmail.com>
2022-11-14 19:27:31 -0300

    chore: create side container of the last films

---
+++ web/src/components/LastFilms/index.tsx
@@ -0,0 +1,44 @@
+import { Star } from "phosphor-react"
+
+export function LastFilms() {
+    return (
+        <div className="bg-zinc-800 h-full w-56 flex flex-col items-center fixed right-0 gap-8 px-4 py-8 mr-1 border-x-[1px] border-x-zinc-600 overflow-y-scroll scrollbar-thumb-zinc-600 scrollbar-track-transparent scrollbar-thin">
+            <div className="flex flex-col">
+                <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/npOnzAbLh6VOIu3naU5QaEcTepo.jpg" />
+                <h1 className="text-lg text-white font-medium mt-2">Castle in the Sky</h1>
+                <h2 className="text-xs text-white font-normal">2019</h2>
+                <div className="flex my-2">
+                    <Star size={20} weight="fill" color="#285CE3" />
+                    <Star size={20} weight="fill" color="#285CE3" />
+                    <Star size={20} weight="fill" color="#285CE3" />
+                    <Star size={20} weight="fill" color="#285CE3" />
+                    <Star size={20} color="#285CE3" />
+                </div>
+            </div>
+            <div className="flex flex-col">
+                <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/npOnzAbLh6VOIu3naU5QaEcTepo.jpg" />
+                <h1 className="text-lg text-white font-medium mt-2">Castle in the Sky</h1>
+                <h2 className="text-xs text-white font-normal">2019</h2>
+                <div className="flex my-2">
+                    <Star size={20} weight="fill" color="#285CE3" />
+                    <Star size={20} weight="fill" color="#285CE3" />
+                    <Star size={20} weight="fill" color="#285CE3" />
+                    <Star size={20} weight="fill" color="#285CE3" />
+                    <Star size={20} color="#285CE3" />
+                </div>
+            </div>
+            <div className="flex flex-col">
+                <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/npOnzAbLh6VOIu3naU5QaEcTepo.jpg" />
+                <h1 className="text-lg text-white font-medium mt-2">Castle in the Sky</h1>
+                <h2 className="text-xs text-white font-normal">2019</h2>
+                <div className="flex my-2">
+                    <Star size={20} weight="fill" color="#285CE3" />
+                    <Star size={20} weight="fill" color="#285CE3" />
+                    <Star size={20} weight="fill" color="#285CE3" />
+                    <Star size={20} weight="fill" color="#285CE3" />
+                    <Star size={20} color="#285CE3" />
+                </div>
+            </div>
+        </div>
+    )
+}
--- web/src/pages/Filmes/index.tsx
+++ web/src/pages/Filmes/index.tsx
@@ -1,13 +1,18 @@
+import { LastFilms } from "../../components/LastFilms";
 import { Sidebar } from "../../components/Sidebar";
 
 export function Filmes() {
     return (
-        <div className='flex items-center justify-between h-screen'>
+        <div className='flex items-center justify-center h-screen relative mr-56 ml-28'>
             <Sidebar />
-            <h1 className="text-7xl font-normal text-white">
-                Hello Filmes!
-            </h1>
-            <div></div>
+
+            <div className="flex flex-col gap-8">
+                <h1 className="text-7xl font-normal text-white">
+                    Hello Filmes!
+                </h1>
+            </div>
+
+            <LastFilms />
         </div>
     )
 }
