b754f827fdb35fe191cab89d195e5a222e77fd6d
MiguelMarcola <miguelmarcola@gmail.com>
2022-11-16 15:55:00 -0300

    chore: create the update button and succesful and fail alerts

--- web/src/components/LastFilms/index.tsx
+++ web/src/components/LastFilms/index.tsx
@@ -3,6 +3,7 @@ import { Star } from "phosphor-react"
 export function LastFilms() {
     return (
         <div className="bg-zinc-800 h-full w-56 flex flex-col items-center fixed right-0 top-0 gap-8 px-4 py-8 mr-1 border-x-[1px] border-x-zinc-600 overflow-y-scroll scrollbar-thumb-zinc-600 scrollbar-track-transparent scrollbar-thin">
+            <h1 className="text-white font-medium text-lg">Top Filmes</h1>
             <div className="flex flex-col">
                 <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/npOnzAbLh6VOIu3naU5QaEcTepo.jpg" />
                 <h1 className="text-lg text-white font-medium mt-2">Castle in the Sky</h1>
